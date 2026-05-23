91bfc6e5f62bd3077775ef9a505f96cca388138c
Navneet Sourath <navneet.gujjar6666@gmail.com>
2026-05-23 21:11:45 +0530

    Update socketFrontEnd.js

--- frontend/src/utils/socketFrontEnd.js
+++ frontend/src/utils/socketFrontEnd.js
@@ -1,10 +1,13 @@
-import io from "socket.io-client";
-import { BASE_URL } from "./constatnts.js";
+import { io } from "socket.io-client";
+import { BASE_URL } from "./constatnts";
 
 export const createSocketConnection = () => {
-  if (location.hostname === "localhost") {
-    return io(BASE_URL);
-  } else {
-    return io("/", { path: "/api/socket.io" });
+  if (window.location.hostname === "localhost") {
+    return io("http://localhost:5000");
   }
-};
+
+  return io(BASE_URL, {
+    transports: ["websocket"],
+    withCredentials: true,
+  });
+}
