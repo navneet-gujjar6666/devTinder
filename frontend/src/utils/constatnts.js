b1014731fb6893988a93aabb7ecee908e6e74f86
Navneet Sourath <navneet.gujjar6666@gmail.com>
2026-05-23 12:37:30 +0530

    Change BASE_URL to production and clean up constants
    Updated BASE_URL to point to the production server and removed commented-out API keys.

--- frontend/src/utils/constatnts.js
+++ frontend/src/utils/constatnts.js
@@ -1,6 +1,3 @@
-export const BASE_URL= "http://localhost:5000";
-
-// export const publicId=rzp_test_SokS7UAbfL2J3i;
-// export const secretId=HVhieCFYS8fu03ZpbaG008n0;
+export const BASE_URL= "https://devtinder-dhpr.onrender.com";
 
 export const VITE_GOOGLE_AI_API_KEY= "AIzaSyCyilYx5uF8hsEF-1y_01foY-6b9yJh0mE";
